b521f8d206ad197ff11aba1fd63608d9ac9e5b8e
Seth Hendrick <seth@shendrick.net>
2018-03-03 23:40:22 -0500

    Added license to source code.

--- SerialPinSetter/CommandParser.ino
+++ SerialPinSetter/CommandParser.ino
@@ -1,3 +1,9 @@
+//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
 
 Command ProcessCommand(const char* str, int strLen)
 {
--- SerialPinSetter/SerialPinSetter.ino
+++ SerialPinSetter/SerialPinSetter.ino
@@ -1,3 +1,10 @@
+//
+//          Copyright Seth Hendrick 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
 const int MIN_PIN_INDEX = 1;
 const int MAX_PIN_INDEX = 13;
 
